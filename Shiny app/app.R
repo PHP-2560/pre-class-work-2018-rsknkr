d1bce7cf5abbc16a7cfe35138fac1891632f9343
Kira Raskina <kira_raskina@brown.edu>
2018-12-05 01:32:42 -0500

    Takes z, returns p

--- Shiny app/app.R
+++ Shiny app/app.R
@@ -1,4 +1,12 @@
 library(shiny)
-ui <- fluidPage()
-server <- function(input, output) {}
+ui <- fluidPage(
+  numericInput(inputId = "n",
+                 "Standard normal statistic", value = 0),
+  textOutput(outputId = "V"),
+  textOutput(outputId = "p")
+)
+server <- function(input, output) {
+  output$p <- reactive({pnorm(input$n)})
+  output$V <- renderText("P value = ")
+}
 shinyApp(ui = ui, server = server)
